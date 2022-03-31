f3dc3813205c4d1fafa0be311f73e9fc7cf7a2ca
Drew2041 <101018333+Drew2041@users.noreply.github.com>
2022-03-31 16:21:35 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,13 +1,13 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <cctype>
 
 unsigned int faStr1(const char *str) {
     int i = 0;
     int figure = 0;
     int inWord = 0;
     int count = 0;
-    while (str[i])
-    {
+    while (str[i]){
         if ((str[i] != ' ') && (str[i] != '/0') && (str[i - 1] == ' ')) {
             count++;
             inWord = 1;
@@ -21,21 +21,20 @@ unsigned int faStr1(const char *str) {
     }
     return count;
 }
-
 unsigned int faStr2(const char *str) {
    int i = 0;
-    int inWord = 0;
-    int count = 0;
-    while (str[i])
-    {
-        if ((str[i] != ' ') && (str[i] != '/0') && (str[i - 1] == ' ') && (isupper(str[i]))) {
-            count++;
-            inWord = 1;
+   int inWord = 0;
+   int count = 0;
+    while (str[i]){
+        if ((str[i] != ' ') && (str[i] != '/0') && 
+            (str[i - 1] == ' ') && (isupper(str[i]))) {
+          count++;
+          inWord = 1;
         }
         i++;
         if (str[i] == ' ')
             inWord = 0;
-        if (((str[i] <97) || (str[i]>122)))
+        if (((str[i] < 97) || (str[i]>122)))
             if (inWord == 1) {
                 count--;
                     inWord = 0;
@@ -43,7 +42,6 @@ unsigned int faStr2(const char *str) {
     }
     return count;
 }
-
 unsigned int faStr3(const char *str) {
    int i = 0;
     int length = 0;
@@ -54,16 +52,13 @@ unsigned int faStr3(const char *str) {
         if ((str[i] != ' ') && (str[i] != '/0') && (str[i - 1] == ' ') ) {
             count++;
             inWord = 1;
-        }
-       
+        }       
         if (inWord == 1)
             length++;
             
         if (str[i] == ' ')
             inWord = 0;
-
         i++;
-
     }
     return length / count;
 }
